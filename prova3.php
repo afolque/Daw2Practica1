43e6eedaeb2cf4324a2e82d62eb43b9438c8481b
maarcc12 <150950675+maarcc12@users.noreply.github.com>
2024-10-07 19:30:31 +0200

    a

--- prova3.php
+++ prova3.php
@@ -1,40 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-
-        $hacer_algo = true;
-
-
-
-    /* No podemos llamar a foo() desde aquí
-    ya que no existe aún,
-    pero podemos llamar a bar() */
-
-        bar();
-
-        if ($hacer_algo) {
-        function foo()
-        {
-            echo "No existo hasta que la ejecución del programa llegue hasta mí.\n";
-        }
-        }
-
-    /* Ahora podemos llamar de forma segura a foo()
-    ya que $hacer_algo se evaluó como verdadero */
-
-        if ($hacer_algo) foo();
-
-        function bar()
-        {
-        echo "Existo desde el momento inmediato que comenzó el programa.\n";
-        }
-
-    ?>
-</body>
-</html>
+<?php
+         $favcolor="verd";
+         switch ($favcolor) {
+          case "vermell":
+          case "VERMELL":
+              echo "el color es vermell!";
+              break;
+          case "blau":
+              echo "el color es blau!";
+              break;
+          case "verd":
+              echo "el color es verd!";
+              break;
+          default:
+              echo "Un altre color!";
+         }
