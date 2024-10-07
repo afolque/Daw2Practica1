3b8459fd537899163c05f29f5818c6b2426e3126
anna <150950573+afolque@users.noreply.github.com>
2024-10-07 19:26:22 +0200

    Update prova3.php

--- prova3.php
+++ prova3.php
@@ -1,40 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<html lang=''>
+   <head>
+      <title></title>
+   </head>
+   <body>
+      <?php
+         $marques=array("Volvo","BMW","Toyota");
+         $dies_mes=array(31,30,31,30,31,30,31,31,30,31);
 
-        $hacer_algo = true;
+         echo "Quin cotxe tens: " . $marques[1] . "<br>";
 
+         $anys= array("Pere"=>"35", "Benito"=>"37", "Joel"=>"43");
+         echo "Anys Pere: " . $anys['Pere'] . "<br>";
+         //echo "Anys per posicio?: " . $anys[2] . "<br>"; //incorrecte
 
+         $info = array(
+            "pere" => array("cotxe"=>"BMW", "anys"=>"35"),
+            "benito" => array("cotxe"=>"Volvo", "anys"=>"23"),
+            "joel" => array("cotxe"=>"Toyota", "anys"=>"75")
+         );
 
-    /* No podemos llamar a foo() desde aquí
-    ya que no existe aún,
-    pero podemos llamar a bar() */
-
-        bar();
-
-        if ($hacer_algo) {
-        function foo()
-        {
-            echo "No existo hasta que la ejecución del programa llegue hasta mí.\n";
-        }
-        }
-
-    /* Ahora podemos llamar de forma segura a foo()
-    ya que $hacer_algo se evaluó como verdadero */
-
-        if ($hacer_algo) foo();
-
-        function bar()
-        {
-        echo "Existo desde el momento inmediato que comenzó el programa.\n";
-        }
-
-    ?>
-</body>
+         echo "El cotxe del pere es: " . $info["pere"]["cotxe"] . "<br>";
+         echo "Les persones son: " . count($info) ."<br>";
+         echo "Les informacions de pere son: " . count($info["pere"]);
+      ?>
+   </body>
 </html>
