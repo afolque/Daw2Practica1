bde616b6b86c26e62ff4486334f214c5a10a5b02
maarcc12 <150950675+maarcc12@users.noreply.github.com>
2024-10-11 16:31:24 +0200

    a

--- prova3.php
+++ prova3.php
@@ -1,16 +1,29 @@
-<?php
-         $favcolor="verd";
-         switch ($favcolor) {
-          case "vermell":
-          case "VERMELL":
-              echo "el color es vermell!";
-              break;
-          case "blau":
-              echo "el color es blau!";
-              break;
-          case "verd":
-              echo "el color es verd!";
-              break;
-          default:
-              echo "Un altre color!";
-         }
+<!DOCTYPE html>
+<html lang=''>
+   <head>
+      <title></title>
+   </head>
+   <body>
+      <?php
+         $marques=array("Volvo","BMW","Toyota");
+         $dies_mes=array(31,30,31,30,31,30,31,31,30,31);
+
+         echo "Quin cotxe tens: " . $marques[5] . "<br>";
+
+         $anys= array("Pere"=>"35", "Benito"=>"37", "Joel"=>"43");
+         echo "Anys Pere: " . $anys['Pere'] . "<br>";
+         //echo "Anys per posicio?: " . $anys[2] . "<br>"; //incorrecte
+
+         $info = array(
+            "pere" => array("cotxe"=>"BMW", "anys"=>"35"),
+            "benito" => array("cotxe"=>"Volvo", "anys"=>"23"),
+            "joel" => array("cotxe"=>"Toyota", "anys"=>"75")
+         );
+
+         echo "El cotxe del pere es: " . $info["pere"]["cotxe"] . "<br>";
+         echo "Les persones son: " . count($info) ."<br>";
+         echo "Les informacions de pere son: " . count($info["pere"]);
+      ?>
+   </body>
+</html>
+
